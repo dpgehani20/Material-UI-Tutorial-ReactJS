f5966c86831abad98a2774c9843c88acf3bf203b
dpgehani20 <69641659+dpgehani20@users.noreply.github.com>
2021-04-28 11:19:03 +0530

    react router

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ import './App.css';
 // import Layoutt from './components/Layoutt'
 // import Tabss from './components/Tabss'
 
-import Productcard from './combination/Productcard'
+// import Productcard from './combination/Productcard'
+
+import Nav from './router/Nav';
+import About from './router/About';
+import Shop from './router/Shop';
+import {BrowserRouter as Router, Switch,Route} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       {/* <h1>React Material UI</h1> */}
       {/* <Buttonc /> */}
@@ -17,9 +23,26 @@ function App() {
       {/* <Layoutt /> */}
       {/* <Tabss /> */}
 
-      <Productcard />
+      {/* <Productcard /> */}
+
+      
+      <Nav />
+      <Switch>
+      <Route path="/" exact component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/shop" component={Shop}/>
+      </Switch>
     </div>
+    </Router>
   );
 }
+
+//route
+
+const Home = () =>(
+  <div>
+    <h2>Home Page</h2>
+  </div>
+);
 
 export default App;
